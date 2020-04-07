9f7c40ecbb1d249b260da40aa59e083f9c074bd9
Cheong min <53283363+keroro1513@users.noreply.github.com>
2020-04-07 15:06:51 +0900

    commit

--- app/src/main/java/kr/co/mktv/stockaibot/MainActivity.kt
+++ app/src/main/java/kr/co/mktv/stockaibot/MainActivity.kt
@@ -1,13 +1,15 @@
 package kr.co.mktv.stockaibot
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TabHost
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
     }
 }
+
